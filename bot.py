c203b3a2652da62b60b8dad85d5a3e879b9d782e
coconutwaffle <100758127+coconutwaffle@users.noreply.github.com>
2024-05-02 13:53:59 +0000

    odified:   bot.py
    한글로 수정됨

--- bot.py
+++ bot.py
@@ -1,26 +1,26 @@
 import random
 
-# This list contains the random responses (you can add your own or translate them into your own language too)
-random_responses = ["That is quite interesting, please tell me more.",
-                    "I see. Do go on.",
-                    "Why do you say that?",
-                    "Funny weather we've been having, isn't it?",
-                    "Let's change the subject.",
-                    "Did you catch the game last night?"]
+# 이 리스트에는 무작위 대답들이 들어있습니다 (원하시면 자신의 언어로 추가하거나 번역할 수 있습니다)
+random_responses = ["정말 흥미롭군요, 더 말씀해주세요.",
+                    "알겠습니다. 계속 말씀해주세요.",
+                    "왜 그렇게 말씀하시나요?",
+                    "요즘 날씨가 참 이상하죠?",
+                    "주제를 바꿔볼까요?",
+                    "어제 저녁 경기 보셨나요?"]
 
-print("Hello, I am Marvin, the simple robot.")
-print("You can end this conversation at any time by typing 'bye'")
-print("After typing each answer, press 'enter'")
-print("How are you today?")
+print("안녕하세요, 저는 마빈이라고 하는 간단한 로봇입니다.")
+print("이 대화를 언제든지 '안녕'이라고 입력하면 종료할 수 있습니다")
+print("답을 입력한 후 '엔터'를 누르세요")
+print("오늘 기분은 어떠세요?")
 
 while True:
-    # wait for the user to enter some text
+    # 사용자의 텍스트 입력을 기다립니다
     user_input = input("> ")
-    if user_input.lower() == "bye":
-        # if they typed in 'bye' (or even BYE, ByE, byE etc.), break out of the loop
+    if user_input == "안녕":
+        # '안녕'을 입력하면 루프에서 벗어납니다
         break
     else:
         response = random.choices(random_responses)[0]
     print(response)
 
-print("It was nice talking to you, goodbye!")
+print("대화하게 되어 기뻤습니다, 안녕히 계세요!")
